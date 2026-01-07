ed212455c283df98da1bed04ae6aa22ffa195c09
akiraazeta <112853568+akiraazeta@users.noreply.github.com>
2026-01-07 22:34:35 +0900

    Update common.js

--- assets/js/common.js
+++ assets/js/common.js
@@ -90,3 +90,19 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 
 });
+
+/* =====================
+   Header Scroll
+===================== */
+
+const header = document.querySelector('.site-header');
+
+if (header) {
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 10) {
+      header.classList.add('is-scrolled');
+    } else {
+      header.classList.remove('is-scrolled');
+    }
+  });
+}
